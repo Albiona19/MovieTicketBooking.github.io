e63be7e5c2150b40a2b0fb560a14fd78cb47d368
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2022-07-03 02:46:35 +0300

    php added

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <title>Cheers Cinema</title>
     <link rel="icon" type="image/png" href="img/logo.png">
 </head>
-
+<!-- php added -->
 <body>
     <?php
     $link = mysqli_connect("localhost", "root", "", "cinema_db");
